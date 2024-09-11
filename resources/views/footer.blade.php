ced5fc1f41c8ee613ba10b556c158a9904d1edd1
Yusufa Haidar <haidardewa@gmail.com>
2024-09-11 13:56:13 +0700

    Making Separate Footer & Header

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,149 @@
+<!doctype html>
+<html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="./assets/favicon/favicon.ico" type="image/x-icon">
+
+    <!-- Map CSS -->
+    <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.css">
+
+    <!-- Libs CSS -->
+    <link rel="stylesheet" href="./assets/css/libs.bundle.css">
+
+    <!-- Theme CSS -->
+    <link rel="stylesheet" href="./assets/css/theme.bundle.css">
+
+    <!-- Title -->
+    <title>Footer</title>
+    </head>
+    
+    <body>
+
+    <!-- SHAPE -->
+    <div class="position-relative">
+        <div class="shape shape-bottom shape-fluid-x text-gray-200">
+          <svg viewBox="0 0 2880 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 48h2880V0h-720C1442.5 52 720 0 720 0H0v48z" fill="currentColor"/></svg>      </div>
+      </div>
+  
+      <!-- FOOTER -->
+      <footer class="py-8 py-md-11 bg-dark">
+        <div class="container">
+          <div class="row">
+            <div class="col-12 col-md-4 col-lg-13">
+  
+              <!-- Brand -->
+              <p class="fw-bold text-white mb-2">
+                P2MPP - Politeknik Negeri Malang
+              </p>
+  
+              <!-- Text -->
+              <p class="text-white mb-2">
+                Gedung Graha Polinema Lantai 3
+              </p>
+              <p class="text-white mb-2">
+                Soekarno-Hatta No.9 Street Malang
+              </p>
+            </div>
+            <div class="col-6 col-md-4 col-lg-2">
+  
+              <!-- Heading -->
+              <h6 class="fw-bold text-uppercase text-gray-700">
+                Lembaga & Portal
+              </h6>
+  
+              <!-- List -->
+              <ul class="list-unstyled text-body-secondary mb-6 mb-md-8 mb-lg-0">
+                <li class="mb-3">
+                  <a href="#!" class="text-reset">
+                    LAM EMBA
+                  </a>
+                </li>
+                <li class="mb-3">
+                  <a href="#!" class="text-reset">
+                    LAM TEKNIK
+                  </a>
+                </li>
+                <li class="mb-3">
+                  <a href="#!" class="text-reset">
+                    Portal Polinema
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-6 col-md-4 col-lg-2">
+  
+              <!-- Heading -->
+              <h6 class="fw-bold text-uppercase text-gray-700">
+                 DATA
+              </h6>
+  
+              <!-- List -->
+              <ul class="list-unstyled text-body-secondary mb-6 mb-md-8 mb-lg-0">
+                <li class="mb-3">
+                  <a href="#!" class="text-reset">
+                    Data Macau
+                  </a>
+                </li>
+                <li class="mb-3">
+                  <a href="#!" class="text-reset">
+                    Data SDY
+                  </a>
+                </li>
+                <li class="mb-3">
+                  <a href="#!" class="text-reset">
+                    Data HK
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-6 col-md-4 col-lg-2">
+  
+                <!-- Heading -->
+                <h6 class="fw-bold text-uppercase text-gray-700">
+                   Social Media
+                </h6>
+
+                <!-- Social -->
+                <ul class="list-unstyled list-inline list-social mb-6 mb-md-0">
+                    <li class="list-inline-item list-social-item me-3">
+                    <a href="#!" class="text-decoration-none">
+                        <img src="./assets/img/icons/social/instagram.svg" class="list-social-icon" alt="...">
+                    </a>
+                    </li>
+                    <li class="list-inline-item list-social-item me-3">
+                    <a href="#!" class="text-decoration-none">
+                        <img src="./assets/img/icons/social/facebook.svg" class="list-social-icon" alt="...">
+                    </a>
+                    </li>
+                    <li class="list-inline-item list-social-item me-3">
+                    <a href="#!" class="text-decoration-none">
+                        <img src="./assets/img/icons/social/twitter.svg" class="list-social-icon" alt="...">
+                    </a>
+                    </li>
+                    <li class="list-inline-item list-social-item">
+                    <a href="#!" class="text-decoration-none">
+                        <img src="./assets/img/icons/social/pinterest.svg" class="list-social-icon" alt="...">
+                    </a>
+                    </li>
+                </ul>
+            </div>
+            </div>
+          </div> <!-- / .row -->
+        </div> <!-- / .container -->
+    </footer>
+
+    <!-- JAVASCRIPT -->
+    <!-- Map JS -->
+    <script src='https://api.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.js'></script>
+
+    <!-- Vendor JS -->
+    <script src="./assets/js/vendor.bundle.js"></script>
+
+    <!-- Theme JS -->
+    <script src="./assets/js/theme.bundle.js"></script>
+
+    </body>
+</html>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,203 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="./assets/favicon/favicon.ico" type="image/x-icon">
+
+    <!-- Map CSS -->
+    <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.css">
+
+    <!-- Libs CSS -->
+    <link rel="stylesheet" href="./assets/css/libs.bundle.css">
+
+    <!-- Theme CSS -->
+    <link rel="stylesheet" href="./assets/css/theme.bundle.css">
+
+    <!-- Title -->
+    <title>Header</title>
+  </head>
+  <body>
+
+    <!-- NAVBAR -->
+    <nav class="navbar navbar-expand-lg bg-white">
+        <div class="container">
+  
+          <!-- Brand -->
+          <a class="navbar-brand" href="./index.html">
+            <img src="./assets/img/brand.png" class="navbar-brand-img" alt="...">
+          </a>
+  
+          <!-- Toggler -->
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+  
+          <!-- Collapse -->
+          <div class="collapse navbar-collapse" id="navbarCollapse">
+  
+            <!-- Toggler -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+              <i class="fe fe-x"></i>
+            </button>
+  
+            <!-- Navigation -->
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item">
+                <a class="nav-link" id="navbarBeranda" href="#" aria-haspopup="true" aria-expanded="false">
+                  Beranda
+                </a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarProfil" data-bs-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false">
+                  Profil
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarProfil">
+                  <div class="row gx-0">
+                    <div class="col-1">
+                      <div class="row gx-0">
+                        <div class="col-5 col-lg-2">  
+                          <!-- List -->
+                          <a class="dropdown-item" href="./profil/visimisi.html">
+                            Visi Misi
+                          </a>
+                          <a class="dropdown-item" href="./profil/kebijakanmutu.html">
+                            Kebijakan Mutu
+                          </a>
+                          <a class="dropdown-item" href="./profil/tugasdanfungsi.html">
+                            Tugas dan Fungsi Utama
+                          </a>
+                        </div>
+                    </div>
+                    </div>
+                  </div>
+                </div> <!-- / .row -->
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarSPMI" data-bs-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false">
+                  SPMI
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarProfil">
+                    <div class="row gx-0">
+                      <div class="col-1">
+                        <div class="row gx-0">
+                          <div class="col-5 col-lg-2">  
+                            <!-- List -->
+                            <a class="dropdown-item" href="./Profil/standar.html">
+                              Standar Kualitas Internal
+                            </a>
+                            <a class="dropdown-item" href="./Profil/spmi.html">
+                              File SPMI
+                            </a>
+                          </div>
+                      </div>
+                      </div>
+                    </div>
+                  </div>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" id="navbarAkreditasi" data-bs-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false">
+                  Akreditasi
+                </a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarPelayanan" data-bs-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false">
+                  Pelayanan
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarPelayanan">
+                  <li class="dropdown-item dropend">
+                    <a class="dropdown-link dropdown-toggle" data-bs-toggle="dropdown" href="#">
+                      Kepuasan Pelanggan
+                    </a>
+                    <div class="dropdown-menu">
+                      <a class="dropdown-item" href="./Pelayanan/Kepuasan Pelanggan/mahasiswa.html">
+                        Mahasiswa
+                      </a>
+                      <a class="dropdown-item" href="./Pelayanan/Kepuasan Pelanggan/orangtua.html">
+                        Orangtua
+                      </a>
+                      <a class="dropdown-item" href="./Pelayanan/Kepuasan Pelanggan/dosenstaff.html">
+                        Dosen & Staff Pendidikan
+                      </a>
+                      <a class="dropdown-item" href="./Pelayanan/Kepuasan Pelanggan/mitra.html">
+                        Mitra
+                      </a>
+                      <a class="dropdown-item" href="./Pelayanan/Kepuasan Pelanggan/alumni.html">
+                        Alumni
+                      </a>
+                    </div>
+                  </li>
+                  <li class="dropdown-item dropend">
+                    <a class="dropdown-link dropdown-toggle" data-bs-toggle="dropdown" href="#">
+                      Pelatihan
+                    </a>
+                    <div class="dropdown-menu">
+                      <a class="dropdown-item" href="./Pelayanan/Pelatihan/pekerti.html">
+                        Pekerti
+                      </a>
+                      <a class="dropdown-item" href="./Pelayanan/Pelatihan/approach.html">
+                        Applied Approach
+                      </a>
+                      <a class="dropdown-item" href="./Pelayanan/Pelatihan/mediadosen.html">
+                        Perangkat & Media Pembelajaran Dosen
+                      </a>
+                      <a class="dropdown-item" href="./Pelayanan/Pelatihan/kurikulum.html">
+                        Penyusunan Kurikulum
+                      </a>
+                    </div>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarSIJAMU" data-bs-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false">
+                  SIJAMU
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarSIJAMU">
+                    <div class="row gx-0">
+                      <div class="col-1">
+                        <div class="row gx-0">
+                          <div class="col-5 col-lg-2">  
+                            <!-- List -->
+                            <a class="dropdown-item" href="./SIJAMU/kalumni.html">
+                                Kuesioner Alumni
+                            </a>
+                            <a class="dropdown-item" href="./SIJAMU/audit.html">
+                                Audit Internal
+                            </a>
+                            <a class="dropdown-item" href="./SIJAMU/dckjm.html">
+                                DCKJM
+                            </a>
+                            <a class="dropdown-item" href="./SIJAMU/kmitra.html">
+                                Kuesioner Mitra
+                            </a>
+                          </div>
+                      </div>
+                      </div>
+                    </div>
+                  </div>
+              </li>
+            </ul>
+  
+            <!-- Button -->
+            <a class="navbar-btn btn btn-sm btn-primary lift ms-auto" href="https://themes.getbootstrap.com/product/landkit/" target="_blank">
+              Masuk
+            </a>
+  
+          </div>
+  
+        </div>
+      </nav>
+    <!-- JAVASCRIPT -->
+    <!-- Map JS -->
+    <script src='https://api.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.js'></script>
+
+    <!-- Vendor JS -->
+    <script src="./assets/js/vendor.bundle.js"></script>
+
+    <!-- Theme JS -->
+    <script src="./assets/js/theme.bundle.js"></script>
+
+  </body>
+</html>
