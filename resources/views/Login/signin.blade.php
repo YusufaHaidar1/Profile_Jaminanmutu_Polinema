c96c2ae7494b4f405ada88c4395385c01af5df98
mochamad rizaldy <mohammadrizaldy16@gmail.com>
2024-09-12 10:20:16 +0700

    kurang gambar

--- resources/views/Login/signin.blade.php
+++ resources/views/Login/signin.blade.php
@@ -27,7 +27,7 @@
 
         h1 {
             text-align: center;
-            margin-bottom: 20px;
+            margin-bottom: 16px;
         }
 
         h2 {
@@ -72,7 +72,7 @@
             text-align: center;
         }
         
-        button {
+        /* button {
             background-color: #CECECE;
             color: #fff;
             padding: 10px 20px;
@@ -80,6 +80,25 @@
             border-radius: 3px;
             cursor: pointer;
             width: 50%;
+        } */
+        .button {
+            background-color: #CECECE; 
+            border: none;
+            color: white;
+            padding: 10px 20px;
+            text-align: center;
+            text-decoration: none;
+            border-radius: 8px; 
+            display: inline-block;
+            font-size: 16px;
+            width: 50%;
+            margin: 4px 2px;
+            cursor: pointer;
+        }
+
+        .button:hover {
+            background-color: #F5C149;
+            color: #fff;
         }
 
         a {
@@ -112,28 +131,32 @@
     <header class="header">
         
     </header>
-
+    
     <div class="container">
         <h1>Masuk</h1>
         <h2>Silahkan masuk terlebih dahulu!</h2>
         <form class="max-w-sm mx-auto">
             <div class="mb-5">
                 <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Masukkan email anda" required />
+                <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Masukkan email " required />
             </div>
             <div class="mb-5">
                 <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kata Sandi</label>
-                <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Masukkan kata sandi anda anda" required />
+                <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Masukkan kata sandi " required />
             </div>
             <div class="remember">
                 <label for="remember" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Lupa Kata sandi?</label>
             </div>
-            <div class="button-container">
+            {{-- <div class="button-container">
                 <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Masuk</button>
+            </div> --}}
+            <div class="button-container">
+                <button class="button">Masuk</button>
             </div>
             <div class="signup">
                 <span>belum menjadi angota ?</span><a href=""> Daftar sekarang</a> 
             </div>
+            
         </form>
     </div>
 
